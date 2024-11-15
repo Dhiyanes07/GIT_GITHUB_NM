60a7431ac1704df68fb0691838ff4fc1c3a7ec8a
Dhiyaneswaran .V <148244277+Dhiyanes07@users.noreply.github.com>
2024-11-15 16:15:12 +0530

    Add files via upload

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/bry02.jpg";
+import Margherita from "../assets/bry03.jpg";
+import PedroTechSpecial from "../assets/bry04.jpg";
+import Vegan from "../assets/bry05.jpg";
+import Pineapple from "../assets/bry06.jpg";
+import Expensive from "../assets/bry07.jpg";
+
+export const MenuList = [
+  {
+    name: "chicken briyani",
+    image: bry02,
+    price: 159.99,
+  },
+  {
+    name: "plain briyani",
+    image: bry03,
+    price: 119.99,
+  },
+  {
+    name: "egg briyani",
+    image: bry04,
+    price: 156.53,
+  },
+  {
+    name: "parwn briyani",
+    image: bry05,
+    price: 379.99,
+  },
+  {
+    name: "mutton briyani",
+    image: bry06,
+    price: 349.99,
+  },
+  {
+    name: "Very Expensive briyani",
+    image: bry07,
+    price: 1999.99,
+  },
+];
